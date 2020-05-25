aeed53ce874d7cccee162d32c6af0f00fb6cb33e
李玉昭 <liyuzhao@yuewen.com>
2020-05-25 10:40:34 +0800

    feature/第一版

--- webpack/webpack.base.config.js
+++ webpack/webpack.base.config.js
@@ -19,7 +19,7 @@ module.exports = {
   resolve: {
     // 自动解析拓展，可以在引用文件的时候不用写后缀
     extensions: ['.js', '.vue', '.json'],
-    // 配置别名，避免在结构嵌套过深的情况下出现../../../../xxx这种写法
+    // 配置别名，避免在结构嵌套过深的情况下出现../../../../xxx这种写法sd
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src')
